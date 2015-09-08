b9087c21820d652dd6ed2c09639a6e8a8a0395b2
tegon <ltegon93@gmail.com>
2015-09-08 09:19:50 -0300

    Debug

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,5 +1,7 @@
 module SessionHelper
   def store_command_in_session(message)
+    p 'store_command_in_session', message.text
+    p 'is_command', is_command?(message.text)
     @last_command = session[message.from.id][:last_command] if session[message.from.id]
     session[message.from.id] = { last_command: message.text } if is_command?(message.text)
   end
