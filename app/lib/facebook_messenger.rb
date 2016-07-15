fee68b5555ff6553ae5d0aea0c05cf19c1d9966b
Leonardo Tegon <ltegon93@gmail.com>
2016-07-15 11:55:56 -0300

    init facebook messenger webhook

--- app/lib/facebook_messenger.rb
+++ app/lib/facebook_messenger.rb
@@ -6,7 +6,7 @@ class FacebookMessenger
   base_uri 'https://graph.facebook.com/v2.6/me/messages'
   headers 'Content-Type' => 'application/json'
   debug_output $stderr
-  default_params { access_token: ENV['MESSENGER_ACCESS_TOKEN'] }
+  default_params 'access_token' => ENV['MESSENGER_ACCESS_TOKEN']
 
   def send(message_data)
     response = self.class.post(body: message_data)
